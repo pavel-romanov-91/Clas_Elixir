be5d413ac9a4a7aebc7818895a785b7ca3b01c77
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2023-11-17 16:08:11 +0000

    sdf

--- main.exs
+++ main.exs
@@ -3,12 +3,15 @@ defmodule Clas_Elixir do
 
 
 def atters do
-IO.puts(@pi)
+  IO.puts(@pi)
 end
 
 
 def point_info({:point,x,y}) do
-IO.puts(x)
-IO.puts(y)
+  some_list = ["london", "Moscow", "Shakhty"]
+  [head | tail] = some_list
+  IO.puts(x)
+  IO.puts(y)
 end
 
+end
