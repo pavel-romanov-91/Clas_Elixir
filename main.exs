962b1cdacf80db8f0da34b21cbe44b3738de283e
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2023-11-17 18:06:22 +0000

    a

--- main.exs
+++ main.exs
@@ -21,4 +21,27 @@ defmodule Clas_Elixir do
     first + second
   end
 
+  def map_work do
+    my_map = %{:name => "Alex", :age => 43}
+    #my_map.name
+    #IO.puts(Map.get(my_map, :name, "not found"))
+    #IO.puts(my_map[:name])
+    #IO.puts(my_map[:age])
+    IO.puts("#{my_map.name} #{my_map.age}")
+    new_map = Map.put(my_map, :lastname, "Voisha")
+    IO.puts(inspect(my_map))
+    IO.puts(inspect(new_map))
+    new_map1 = Map.delete(new_map, :lastname)
+    IO.puts(inspect(new_map1))
+  end
+
+  def user_age({:user, _, age}) do
+    
+    age
+  end
+
+  def user_f do
+
+  end
+
 end
