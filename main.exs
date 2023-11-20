c1a3ed2eb66e11a3e477bcd0445ae1c2c0922792
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2023-11-20 18:09:29 +0000

    g

--- main.exs
+++ main.exs
@@ -35,12 +35,85 @@ defmodule Clas_Elixir do
     IO.puts(inspect(new_map1))
   end
 
-  def get_age({:user, age, _}) do
+  def get_age({tule_value}) do
+    {:user, age, _} = tule_value
      age
   end
 
   def get_names({:user, name1,_},{:user, name2,_},{:user, name3,_}) do
     [name1, name2, name3]
   end
+
+  def if_work(color) do
+    if color == "blue" do
+      "Yes"
+      end
+    end
+
+  def if_short(color) do
+  if color == "blue", do: "Yes"
+  end
+
+  def unless_work(color) do
+    unless color == "blue" do
+    "Yes"
+    end
+  end
+
+  def unless_short(color) do
+    unless color == "blue", do: "Yes"
+  end
+
+  def if_work2(color) do
+  if color == "blue" do
+    "Yes"
+    else
+    "No"
+    end
+  end
+
+  def if_short2(color) do
+    if color == "blue", do: "Yes", else: "No"
+  end
+
+  def case_nod(a, b) do
+    case rem(a, b) do
+      0 -> b
+      c -> case_nod(b, c)
+    end
+  end
+
+  def user({:user, age, name}) do
+    value_if = if age >= 18, do: "привет проходи #{name}", else: "привет уходи #{name}"
+    value_if
+  end
+
+  def case_user (user) do
+    case user do
+      {:user, age, name} when age >= 18 -> "привет проходи #{name}"
+      {:user, _, name} -> "привет уходи #{name}"
+      _ -> "Не коректные данные"
+    end
+  end
+
+  def case_user1 (user) do
+    case user do
+      {:user, name, age, role} when age >= 18 -> "привет проходи #{name}"
+      {:user, _, _, :admin} -> :ok
+      {:user, _, _, :moderator} -> :ok
+      {:user, _, name} -> "привет уходи #{name}"
+      _ -> "Не коректные данные"
+    end
+  end
+
+  def f(curent_color, figure) do
+    case {curent_color, figure} do
+      {curent_color, {color, type}} when (curent_color == color) and (type == :pawn or type == :rock) -> "Yes"
+      _ -> "No"
+      
+    end
+
+  end
+
 
 end
